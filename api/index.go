c2ffb9bdc929c68f5631944f70e187efb1b2c4e5
Alvaro <alvaro.nrx88@gmail.com>
2025-01-04 18:41:47 -0300

    api fix middleware

--- api/index.go
+++ api/index.go
@@ -4,10 +4,20 @@ import (
 	"fmt"
 	"net/http"
 	"go-rapido/api/middleware"
+	"strings"
 )
 
 func Handler(w http.ResponseWriter, r *http.Request) {
-	middleware.LoggingMiddleware(r)
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+	
+	path := r.URL.Path
+
+
+	if path == "" || path == "/" {
+		path = "/index.html"
+	}
+	middleware.LoggingMiddleware(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {})).ServeHTTP(w, r)
 
 	fmt.Fprintf(w, "Hello, %s!", r.URL.Path[1:])
 }
